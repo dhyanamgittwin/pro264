3dcee9e5f8c7cfaa5ca8f283273a329d5d8aee44
Dhyanam <dhyanam2412@gmail.com>
2023-12-31 09:20:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Import Libraries below
+import os
+import cv2
+from flask import Flask, render_template, request, redirect, url_for, send_file
+from werkzeug.utils import secure_filename
+
+# Define flask 
+app = Flask(__name__)
+@app.route('/')
+# Define upload_form() and route the webapp 
+def upload_form():
+	return render_template('upload.html')
+# Define upload_video() to get video in defined folder and route the webapp  
+@app.route('/', methods=['POST'])
+def upload_video():
+	file = request.files['file']
+	filename = secure_filename(file.filename)
+	file.save(os.path.join('static/', filename))
+	return render_template("upload.html", filename=filename)
+
+@app.route('/display/<filename>')
+# Define display_video() to Display video in defined folder and route the webapp  
+def display_video(filename):
+	return redirect(url_for('static', filename=filename))
+	
+if __name__ == "__main__":
+    app.run(debug=True)
